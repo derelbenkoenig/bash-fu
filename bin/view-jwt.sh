b707ccc311de9fbe5869d9588ad0dc276c91dfe0
Robert Benson <derelbenkoenig@gmail.com>
2024-05-31 18:04:04 -0400

    update view-jwt to be nicer

--- bin/view-jwt.sh
+++ bin/view-jwt.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env jq -R -f
 
 # JWTs are encoded in URL-safe base64, which replaces + and / with - and _ respectively
 # perform the inverse of that here since jq's @base64d only understands the default version of base64
-jq -R '
-    split(".")
-    | .[0:2]
-    | map(
-        sub("-"; "+")
-        | sub("_"; "/")
-        | @base64d
-        | fromjson
-    )
-    | .[1] |= (
-        def addDecodedTs($k): .["_" + $k + "_decoded"] = (.[$k] | todate);
-        reduce ("iat", "nbf", "exp") as $tsk (.;addDecodedTs($tsk))
-    )'
+split(".")[0:2]
+| map(
+    sub("-"; "+")
+    | sub("_"; "/")
+    | @base64d
+    | fromjson
+)
+| .[1] |= (
+    def addDecodedTs($k): .["_\($k)_decoded"] = (.[$k] | todate);
+    reduce ("iat", "nbf", "exp") as $tsk (.;addDecodedTs($tsk))
+)
